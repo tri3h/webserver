2eb1d989e929e7d78b7464713dffa4532a981bb6
tri3h <tri3h@ya.ru>
2022-04-27 11:55:58 +0500

    Fix add_minor_photo script

--- scripts/add_minor_photo.sh
+++ scripts/add_minor_photo.sh
@@ -20,8 +20,6 @@ source utility/load_config.sh
 
 base64 $minor_photo > "image.dat"
 
-cat "image.dat"
-
 curl "$host:$port/drafts/minor_photo?token=$token&draft_id=$draft_id&image_type=$minor_photo_image_type" -X POST -F minor_photo=@image.dat
 
 rm "image.dat"
